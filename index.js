9d0065b4be01317493e8fe955538145c1a694347
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-19 22:01:56 +0530

    registering playBackService with TrackPlayer.

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import TrackPlayer from 'react-native-track-player';
 import App from './src/App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { playBackService } from './musicPlayerServices';
+
+// AppRegistry.registerComponent(...);
+TrackPlayer.registerPlaybackService(() => playBackService);
 
 AppRegistry.registerComponent(appName, () => App);
