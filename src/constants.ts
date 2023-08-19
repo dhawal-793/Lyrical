7a5efa2907ccf8c9941c931ec9925c7010a37ff2
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-19 21:58:27 +0530

    created constants to store dummy songs data.

---
+++ src/constants.ts
@@ -0,0 +1,44 @@
+import { Track } from 'react-native-track-player'
+
+export const playListData: Track[] = [
+    {
+        id: 1,
+        title: 'Cheap Thrills',
+        artist: 'Sia',
+        album: 'This Is Acting',
+        artwork: require('./assets/images/Cheap_Thrills.png'),
+        url: require('./assets/audios/Cheap_Thrills.mp3'),
+    },
+    {
+        id: 2,
+        title: 'On My Way',
+        artist: 'Farruko, Alan Walker, Sabrina Carpenter',
+        album: 'Live Fast',
+        artwork: require('./assets/images/On_My_Way.png'),
+        url: require('./assets/audios/On_My_Way.mp3'),
+    },
+    {
+        id: 3,
+        title: 'In The End',
+        artist: 'Linkin Park',
+        album: 'Hybrid Theory',
+        artwork: require('./assets/images/In_The_End.jpg'),
+        url: require('./assets/audios/In_The_End.mp3'),
+    },
+    {
+        id: 4,
+        title: 'Teri Mitti',
+        artist: 'B Prak, Arko',
+        album: 'Kesari SoundTrack',
+        artwork: require('./assets/images/Teri_Mitti.jpg'),
+        url: require('./assets/audios/Teri_Mitti.mp3'),
+    },
+    {
+        id: 5,
+        title: 'Faded',
+        artist: 'Alan Walker',
+        album: 'Different World',
+        artwork: require('./assets/images/Faded.jpg'),
+        url: require('./assets/audios/Faded.mp3'),
+    },
+]
