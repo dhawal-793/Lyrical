4ba2bcb16a74ae3e31d249268cee7613360a0fac
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-20 21:13:35 +0530

    Created SongSlider component.

---
+++ src/components/SongSlider.tsx
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+const SongSlider = () => {
+    return (
+        <View>
+            <Text>SongSlider</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default SongSlider
