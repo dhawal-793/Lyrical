368c5f61d0c1a575ae7858e35cafd4c9997717ab
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-20 20:42:18 +0530

    Created ControlCenter component to handle controls

---
+++ src/components/CcontrolCenter.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+
+
+const CcontrolCenter = () => {
+    return (
+        <View>
+            <Text>ControlCenter</Text>
+        </View>
+    )
+}
+
+export default CcontrolCenter
