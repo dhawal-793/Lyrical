44361f35eea55affcf6cc6ef1c7f7c713f737d3b
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-20 21:19:32 +0530

    Added Slider with time, duration, etc.

--- src/components/SongSlider.tsx
+++ src/components/SongSlider.tsx
@@ -1,9 +1,28 @@
+import Slider from '@react-native-community/slider'
 import { View, Text, StyleSheet } from 'react-native'
+import { useProgress } from 'react-native-track-player'
 
 const SongSlider = () => {
+
+    const { duration, position } = useProgress()
+
     return (
         <View>
-            <Text>SongSlider</Text>
+            <Slider
+                value={position}
+                minimumValue={0}
+                maximumValue={duration}
+                thumbTintColor='#FFFFFF'
+                maximumTrackTintColor='#FFFFFF'
+            />
+            <View>
+                <Text>
+                    {new Date(position * 1000).toISOString().substring(15, 19)}
+                </Text>
+                <Text>
+                    {new Date((duration - position) * 1000).toISOString().substring(15, 19)}
+                </Text>
+            </View>
         </View>
     )
 }
