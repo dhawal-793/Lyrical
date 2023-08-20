a11b0275ad7e5cb21fdc0d575ca1f809620c7336
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-20 21:03:18 +0530

    Created SongInfo component to display song details

---
+++ src/components/SongInfo.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, View } from 'react-native'
+
+const SongInfo = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+
+const Styles = StyleSheet.create({
+
+})
+
+export default SongInfo
