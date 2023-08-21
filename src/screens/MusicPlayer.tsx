a7448797ce34192b7eae928a8ec3d7a1e587b958
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-21 21:41:34 +0530

    Added methods and structure for the Player Screen.

--- src/screens/MusicPlayer.tsx
+++ src/screens/MusicPlayer.tsx
@@ -1,11 +1,81 @@
-import { Text, View } from 'react-native'
+import { useState } from 'react'
+import { FlatList, Image, StyleSheet, Text, View, Dimensions } from 'react-native'
+import TrackPlayer, { Track, Event, useTrackPlayerEvents } from 'react-native-track-player'
+import { playListData } from '../constants'
+import CcontrolCenter from '../components/CcontrolCenter'
+import SongSlider from '../components/SongSlider'
+import SongInfo from '../components/SongInfo'
+
+const { width } = Dimensions.get('window')
 
 const MusicPlayer = () => {
+
+    const [track, setTrack] = useState<Track | null>(null)
+
+
+    useTrackPlayerEvents([Event.PlaybackTrackChanged], async event => {
+        switch (event.type) {
+            case Event.PlaybackTrackChanged:
+                const playingTrack = await TrackPlayer.getTrack(event.nextTrack)
+                setTrack(playingTrack)
+                break;
+
+            default:
+                break;
+        }
+    })
+
+    const renderArtWork = () => {
+        return (
+            <View style={styles.listArtWrapper}>
+                <View style={styles.albumContainer}>
+                    {track?.artwork && (
+                        <Image
+                            source={{ uri: track?.artwork?.toString() }}
+                            style={styles.albumArtImg}
+                        />
+                    )}
+                </View>
+            </View>
+        )
+    }
+
     return (
-        <View>
-            <Text>MusicPlayer</Text>
+        <View style={styles.container}>
+            <FlatList
+                horizontal
+                data={playListData}
+                renderItem={renderArtWork}
+                keyExtractor={song => song.id.toString()}
+            />
+            <SongInfo track={track} />
+            <SongSlider />
+            <CcontrolCenter />
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#001d23',
+    },
+    listArtWrapper: {
+        width: width,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    albumContainer: {
+        width: 300,
+        height: 300,
+    },
+    albumArtImg: {
+        height: '100%',
+        borderRadius: 4,
+    },
+})
 
 export default MusicPlayer
