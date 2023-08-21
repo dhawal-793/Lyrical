20cf8f34729f7d171cc5bfd217dda2224a5a0f95
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-21 21:45:13 +0530

    Added MusicPlayer Screen in app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
+import { useEffect, useState } from 'react';
 import {
+  ActivityIndicator,
   SafeAreaView,
-  ScrollView,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
 } from 'react-native';
+import { addTrack, setUpPlayer } from '../musicPlayerServices';
+import MusicPlayer from './screens/MusicPlayer';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [isPlayerReady, setIsPlayerReady] = useState(false)
+
+  async function setup() {
+    let isSetup = await setUpPlayer()
+
+    if (isSetup) {
+      await addTrack()
+    }
+
+    setIsPlayerReady(isSetup)
+  }
+
+  useEffect(() => {
+    setup()
+  }, [])
+
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    )
+  }
 
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor: isDarkMode ? '#000' : '#fff' }]}>
-      <ScrollView>
-        <View>
-          <Text style={styles.heading}>Lyrical</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.heading}>Lyrical</Text>
+      </View>
+      <MusicPlayer />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#001d23',
+    paddingTop: 30,
   },
   heading: {
     color: 'green',
-    fontSize: 32,
+    fontSize: 48,
     fontWeight: 'bold',
     textAlign: 'center',
   }
